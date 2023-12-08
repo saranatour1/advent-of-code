3e765968e9d91a1fc42fb2520548ce6b992f0218
saranatour1 <saranatourcontact@gmail.com>
2023-12-09 00:46:27 +0200

    Day 7

--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,51 @@
-import { time } from 'node:console';
-// the lowest location number that corresponds to any of the initial seeds
 import fs, { copyFileSync } from "node:fs";
 import readline from "node:readline";
-
-interface MapData {
-  seeds: (RegExpMatchArray | null);
-  seedToSoil: string[];
-  soilToFertilizer: string[];
-  fertilizerToWater: string[];
-  waterToLightMap: string[];
-  lightToTemperature: string[];
-  temperatureToHumidity: string[];
-  humidity: string[];
-}
+const items = [ "A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"].reverse();
+const hands = ['fiveOfAKind','fourOfAKind','fullHouse','threeOfAKind',"twoPair" ,"onePair",'highCard'];
 
 
-function day6() {
-  // get array of lines
-
+// A hand consists of five cards labeled one of A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2. The relative strength of each card follows this order, where A is the highest and 2 is the lowest.
+function day7() {
   async function getLines() {
-    const fileStream = fs.createReadStream("src/day-6/input.txt");
+    const fileStream = fs.createReadStream("src/day-7/ex.txt");
     const rl = readline.createInterface({
       input: fileStream,
       crlfDelay: Infinity,
     });
-    const arr: number[][] = [];
+    const arr: string[][] = [];
     for await (const line of rl) {
-      const [,linToGet] = line.split(":")
-      let x = linToGet.match(/\d+/g)?.map(item => Number(item));
-      arr.push(x as number[])
+      arr.push(line.split(" "))
     }
     return resolveArray(arr);
   }
   // arr[0] is time arr[1] is distance
-  function resolveArray(arr:number[][]){
-    // how many times can I beat the distance number if  iterate over the time and calculate the resultant distance, and multiply all?
-    let bestExpectedTimes:number[] =[]
-    let part2 = arr.map(nums => nums.join(""))
-  
-
-  arr[0].forEach((currentTime,idx)=>{    
-    let distance;
-    let holdTime; 
-
-    let traveledDistance = arr[1][idx];
-    let numberOfGoodTimes =0;
-    for(let remainingTime =0; remainingTime<=currentTime;remainingTime++){
-      holdTime= currentTime -remainingTime
-      if (remainingTime === 0) {
-      distance = 0;
-      } else {
-        const speed: number = holdTime;
-        distance = speed * remainingTime;
-        
-      }
-      if(distance > traveledDistance){
-        numberOfGoodTimes++;
-      }
+  function resolveArray(arr:string[][]){
+    const handMap = new Map();
+    /* The line `console.log(arr.length)` is printing the length of the `arr` array to the console. */
+    // console.log(arr.length)
+    let charOccurrences = new Array(arr.length).fill(0);
+    for (const line of arr){
+      handMap.set(line[0],Number(line[1]));
     }
-    bestExpectedTimes.push(numberOfGoodTimes);
-  })
-  let numberOfGoodTimesPart2 =0;
-  
-  let currentTime = Number(part2[0])
-  let traveledDistance= Number(part2[1]);
+    
+    for(const [key, bet] of handMap){
+      // console.log(key, bet)
+      let card = new Array()
 
-  let distance;
-  let holdTime; 
-  for(let remainingTime =0; remainingTime<=currentTime;remainingTime++){
-    holdTime= currentTime -remainingTime
-    if (remainingTime === 0) {
-    distance = 0;
-    } else {
-      const speed: number = holdTime;
-      distance = speed * remainingTime;
+      for(const c in key){
+        card.push(items.indexOf(c));
+        charOccurrences[items.indexOf(c)]++;
+      }
+
+      console.log(charOccurrences)
       
+
     }
-    if(distance > traveledDistance){
-      numberOfGoodTimesPart2++;
-    }
+    
+  //  console.log(handMap)
   }
-
-
-  console.log("Part 1", bestExpectedTimes.reduce((accumulator, currentValue) => accumulator * currentValue, 1))
-  console.log("Part 2", numberOfGoodTimesPart2)
-  }
-
 
   getLines()
-
-
 }
 
-day6();
+day7();
