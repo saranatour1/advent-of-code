abc9ae94ec2cfb72826796f7fe00482705b58361
saranatour1 <saranatourcontact@gmail.com>
2024-12-07 07:03:40 +0200

    day-6

--- src/2024/day-6/index.ts
+++ src/2024/day-6/index.ts
@@ -5,135 +5,49 @@ export const day6 = () => {
 
   const path = file.split("\r\n").map((line) => line.split(""));
 
-  let currentGuardPosition: number[] = findCurrentPosition(path)
+  const directions = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+  ];
+  const rows = path.length;
+  const cols = path[0].length;
 
-  // you start at the position of "^"
-  let colIndex = currentGuardPosition[0];
-  let rowIndex = currentGuardPosition[1];
-
-  while(colIndex!=-1 && rowIndex!=-1){
-    // first find the direction it is facing
-    let direction = getNextMove(path[colIndex][rowIndex]);
-    // moving up
-    while(direction === "up"){
-      if(path[colIndex-1][rowIndex] !== "#"){
-        path[colIndex][rowIndex] = "X"
-        path[colIndex-1][rowIndex] = "^"
-        colIndex--;
-        currentGuardPosition = findCurrentPosition(path)
-      }else{
-        // rotate 90 degrees right
-        path[colIndex][rowIndex] = ">"
-        // path[colIndex][rowIndex+1] = ">"
-        currentGuardPosition = findCurrentPosition(path)
-        direction = getNextMove(">")
+  let guardRow: number = 0,
+    guardCol: number = 0,
+    guardDir: number = 0;
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if ("^>v<".includes(path[r][c])) {
+        guardRow = r;
+        guardCol = c;
+        guardDir = "^>v<".indexOf(path[r][c]); // Map direction symbol to index
         break;
       }
     }
-
-    while(direction === "right"){
-      if(path[colIndex][rowIndex+1] !== "#"){
-        path[colIndex][rowIndex] = "X"
-        path[colIndex][rowIndex+1] = ">"
-        rowIndex++;
-        currentGuardPosition = findCurrentPosition(path)
-      }else{
-        // rotate 90 degrees from right down
-        path[colIndex][rowIndex] = "v"
-        // path[colIndex+1][rowIndex] = "v"
-        currentGuardPosition = findCurrentPosition(path)
-        direction = getNextMove("v")
-        break;
-      }
-    }
-
-    while(direction === "down"){
-      if(path[colIndex+1][rowIndex] !== "#"){
-        path[colIndex][rowIndex] = "X"
-        path[colIndex+1][rowIndex] = "v"
-        colIndex++;
-        currentGuardPosition = findCurrentPosition(path)
-      }else{
-        // rotate 90 degrees from right left
-        path[colIndex][rowIndex] ="<"
-        // path[colIndex][rowIndex-1] = "<"
-        currentGuardPosition = findCurrentPosition(path)
-        direction = getNextMove("<")
-        break;
-      }
-    }
-
-    while(direction === "left"){
-      if(path[colIndex][rowIndex-1] !== "#"){
-        path[colIndex][rowIndex] = "X"
-        path[colIndex][rowIndex-1] = "<"
-        rowIndex--;
-        currentGuardPosition = findCurrentPosition(path)
-      }else{
-        // rotate 90 degrees from right up
-        path[colIndex][rowIndex] = "^"
-        // path[colIndex-1][rowIndex] = "^"
-        currentGuardPosition = findCurrentPosition(path)
-        direction = getNextMove("^")
-        break;
-      }
-    }
-
-    // check left right and chose down for being stuck
-    if(path[colIndex][rowIndex-1] ==="X" && path[colIndex][rowIndex+1]==="X"){
-      direction = getNextMove("v")
-    }
-
-    if(path[colIndex+1][rowIndex] ==="X" && path[colIndex-1][rowIndex+1]==="X"){
-      direction = getNextMove(">")
-    }
-
-    if(path[colIndex][rowIndex] === undefined) return
-    console.log(direction)
-    console.log(currentGuardPosition)
-    break
   }
 
-  // // rotation 
-  console.log("The current path","\n\n", path.map(line=>line.join('').trim()).join("\n"));
-  // console.log("current path of the guard", currentGuardPosition);
-};
+  const visited = new Set([`${guardRow},${guardCol}`]);
 
-// current positions to follow are "^" ">" "<" "v"
-const findCurrentPosition = (path: string[][]) => {
-  let current:number[] = [];
-  path.forEach((line, index) => {
-    if (line.indexOf("^") !== -1) {
-      current = [index, line.indexOf("^")];
-    }
-    if (line.indexOf("v") !== -1) {
-      current = [index, line.indexOf("v")];
-    }
-    if (line.indexOf(">") !== -1) {
-      current = [index, line.indexOf(">")];
-    }
-    if (line.indexOf("<") !== -1) {
-      current = [index, line.indexOf("<")];
-    }
-  });
-  return current;
-};
+  while (true) {
+    const [dr, dc] = directions[guardDir];
+    const nextRow = guardRow + dr;
+    const nextCol = guardCol + dc;
 
-const isVisited = (path:string[][],col:number,row:number,)=>{
-  return path[col][row] === "X"
-}
+    // Check if the next position is outside the grid
+    if (nextRow < 0 || nextRow >= rows || nextCol < 0 || nextCol >= cols) {
+      break; // Guard leaves the grid
+    }
 
-const getNextMove =(symbol:string) =>{
-  switch (symbol) {
-      case "^":
-          return "up";
-      case ">":
-          return "right";
-      case "<":
-          return "left";
-      case "v":
-          return "down";
-      default:
-          return
+    if (path[nextRow][nextCol] === "#") {
+      // Obstacle ahead: turn right (clockwise)
+      guardDir = (guardDir + 1) % 4;
+    } else {
+      [guardRow,guardCol] = [nextRow,nextCol]
+      visited.add(`${guardRow},${guardCol}`);
+    }
   }
-}
+
+  console.log(`Distinct positions visited: ${visited.size}`);
+};
